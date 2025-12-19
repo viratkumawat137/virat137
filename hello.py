64100fa6da498537200e0c683b1d8e45d00ccc0f
viratkumawat137 <viratkumawat137@gmail.com>
2025-12-19 14:11:14 +0530

    Update hello.py with example

--- hello.py
+++ hello.py
@@ -1,32 +1,44 @@
 #print("hello \n world")
+#example=print("Show the text 'Hello, World!' \n on the computer screen.")
 
 
 #print("hello\t world")
+#example=print("If the current temperature is greater than \t then tell the user 'It is a hot day.'")
 
-#print("my name is \"virat\".")
+
+#print("my name is \"virat\" kumawat.")
+#example=print("For every \"individual\" item in this list, print its name one by one.")
 
 
 #print('i would love to have a cup of \'coffee\'with you.')
+#example=print("For every \'individual\' item in this list, print its name one by one.")
 
 
 #print("that\\there ")
+#example=print("while\\for")
 
 
 #print("vikas\rahul")
+#example=print("sp.com\ps.com")
+
 
 #print("apple company \r vivo")
-
+#example=print("sahil kumawat \r virat")
 
 
 #print("vipins\b sir")
-
+#example=print("virats\b kumar")
 
 
 #print("hello \v world")
+#example=print("zake \v david")
 
 
+#print("\a ")
+example=print("Python Code Explained \a in English")
 
 
-#print("\a")
+			
+
 
 			
