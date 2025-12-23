bfb95ea02322c51f4941cb0096149a0efc700f19
virat kumawat <viratkumawat137@gmail.com>
2025-12-23 15:41:21 +0530

    ifelse practice file

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+'''a=int(input("enter your age"))
+if a >= 18:
+	print("you are eligible for vote")
+	print("thank you")
+else:
+	print("you are not eligible for vote")
+	print("Come back when you're eligible ")   '''
+
+###
+
+b=int(input("enter your amount"))
+
+if b >=100:
+	print("You can withdraw Rupees")
+else:
+	print("you can not withdraw rupess")                                                
+	print("come again another day")  ###
+
