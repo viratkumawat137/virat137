de97368b08284df86fb72d8b1bcff9a99541d53b
virat kumawat <viratkumawat137@gmail.com>
2026-01-06 02:08:35 +0530

    revision of if_else

---
+++ ifelse.review.py
@@ -0,0 +1,113 @@
+'''a = int(input("Enter a number: "))
+if a >= 10:
+    print(True)
+else:
+    print(False)'''
+
+
+
+'''
+b=int(input ("a enter number"))
+if b <= 10:
+	print(True)
+else:
+	print(False)'''
+
+
+'''
+
+a=int(input("enter a number"))
+if a >= 100:
+	print("yes")
+else:
+	print("no") '''
+'''
+
+a=int(input("enter number"))
+if a % 2==0:
+	print("sum number hai")
+else:
+	print("odd number hai") '''
+
+'''
+
+a=int(input("enter score "))
+if a >= 90:
+	print("1st Rank ") 
+elif a >=75:
+	print("2nd Rank")
+elif a >=55:
+	print("3rd Rank")
+elif a >=45:
+	print("selected")
+else:
+	print("not selected")  '''
+'''
+
+user = "admin"
+passwords = "123"
+user=input("enter a username")
+password=input("enter a password")
+if user==user and password==passwords:
+	print("login succesfully")
+else:
+	print("faild")  ''' 
+
+'''
+
+c=int(input("enter number"))
+if c == 1:
+	print("monday")
+if c == 2:
+	print("tue....")
+
+if c == 3:
+	print("wen...")
+
+if c == 4:
+	print("thu...")
+
+if c == 5:
+	print("fri...")
+
+if c == 6:
+	print("sat...")
+
+if c == 7:
+	print("sunday")
+
+if c > 7 or c < 1 :
+	print("not match")    '''
+
+
+'''
+
+
+amount = int(input("Enter value: "))
+
+match amount:
+    case 10:
+        print("mount dew")
+    case 20:
+        print("fenta")
+    case 30:
+        print("Coca-Cola")
+    case 40:
+        print("Thums Up")
+    case 50:
+        print("Pepsi")
+    case 60:
+        print("Campa Cola")
+    case 70:
+        print("7Up")
+    case _:
+        print("no coldrink")
+'''
+
+
+
+
+	
+
+
+
