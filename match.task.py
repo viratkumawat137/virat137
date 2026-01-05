08acdeee74b6455e3e04acebf71a4efaf0bc02b4
virat kumawat <viratkumawat137@gmail.com>
2026-01-06 02:21:55 +0530

    match task

---
+++ match.task.py
@@ -0,0 +1,13 @@
+a=int(input("some word write"))
+b=int(input("some word write"))
+c=input("which oprater")
+if print(a+b):
+	print("check now")
+if print(a-b):
+	print("check now")
+if print(a*b):
+	print("check now")
+if print(a%b):
+	print("check now")
+if print(a/b):
+	print("check now")
