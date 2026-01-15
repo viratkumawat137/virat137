75a22294b47fecc0ceda7625adb9ad18c2e95ec2
virat kumawat <viratkumawat137@gmail.com>
2026-01-15 18:51:49 +0530

    while 1st class

---
+++ while.py
@@ -0,0 +1,23 @@
+###
+a = int(input("type your age: "))
+while a < 20:
+    print("you not eligible")
+	a += 20: ###
+  
+###num = int(input("type number (e.g. 1): "))
+while num <= 5:
+    print(num)
+    num += 1  ###
+
+
+value = int(input("type number (e.g. 10): "))
+while value >= 5:
+    print(value)
+    val -= 1
+
+
+
+
+
+
+	
