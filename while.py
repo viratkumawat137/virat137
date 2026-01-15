4319c41697c928f8f4b40dc1608101524b8e926c
virat kumawat <viratkumawat137@gmail.com>
2026-01-16 00:36:52 +0530

    while practice

--- while.py
+++ while.py
@@ -1,23 +1,24 @@
-###
-a = int(input("type your age: "))
-while a < 20:
-    print("you not eligible")
-	a += 20: ###
-  
-###num = int(input("type number (e.g. 1): "))
+'''
+num = int(input("type number  "))
 while num <= 5:
     print(num)
-    num += 1  ###
+    num += 1 '''
+'''
 
-
-value = int(input("type number (e.g. 10): "))
+value = int(input("type number  "))
 while value >= 5:
     print(value)
-    val -= 1
+    value -= 1'''
 
 
+'''
+a=1
+while a < 10:
+	print(a)
+	a +=1 '''
 
-
-
-
-	
+'''
+a=int(input("type kro sir ji "))
+while a <= 10:
+	print(a)
+	a +=1  '''
