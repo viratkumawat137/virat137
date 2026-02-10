2be3f0f1112457f72dd363b74f6668a41c77abde
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-10 12:08:09 +0530

    escape.html

---
+++ Escape_Sequences.py
@@ -0,0 +1,7 @@
+print("hello\n world")= new line 
+print("hello\v world")=form feed 
+print("hello\f world")=form feed 
+print("hello\t world")=hello world 
+print("helloo\b world")=backspace==>hello
+print("hello\a world")=beep sound
+print("hello\\ world")=backslase==>/
