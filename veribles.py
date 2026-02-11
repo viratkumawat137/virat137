613fca2874ee4dccf857f77df435e76a994b2647
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-11 16:26:57 +0530

    Add files via upload

---
+++ veribles.py
@@ -0,0 +1,26 @@
+"""
+a=30
+print(a)
+a=30.2
+print(a)
+
+
+a="virat"
+print(a) """
+
+#print(type(34))
+#print(type(73.5))
+#print(type(True))
+#print(type("saj\hil"))
+
+
+""" ab=67
+print(type(ab))  """
+
+
+#Assignment===>
+
+"""
+a=b=c=d=e=90
+print(a ,b ,c ,d ,e )
+"""
