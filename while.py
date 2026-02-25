54e8a4d736f556b5a9ad919cb5be79099ff933c4
Sahil kumawat <viratkumawat137@gmail.com>
2026-02-25 21:40:08 +0530

    Refactor while loops and input handling in while.py

--- while.py
+++ while.py
@@ -23,4 +23,74 @@ while a <= 10:
 	print(a)
 	a +=1  '''
 
+"""
+while True:
+    print("Hello World")
+    print("Hello World")  
+                """
+
+
+"""
+counter=10
+while counter :
+    print(counter)
+    counter = counter - 1   """
+
+#iterations
+"""
+counter=1000
+while counter :
+    print(counter)
+    counter = counter - 1 
+    """
+
+"""counter="vipin"
+while counter :
+    print(counter)
+    counter = counter[:-1]
+
+"""
+"""counter=1
+while counter <= 10:
+    print(counter)
+    counter += 1
+"""
+
+#task 
+"""
+counter=1
+while counter <= 50:
+    print(counter)
+    counter += 1  
+
+    """
+
+"""
+counter=50  
+while counter :
+    print(counter)
+    counter -= 1     
+      """
+"""
+num=5
+i=1
+while i<=10:
+    print(num*i)
+    i+=1 """
+
+
+
+num=5
+i=1 
+while i<=10:
+    print(num,"*",i,"=",num*i)
+    i+=1
+
+
+
+
+
+
+
+
 
