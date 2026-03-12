f2e2c3a704a1d0e0fa3a57cc86f7b679c8c3ed52
Sahil kumawat <viratkumawat137@gmail.com>
2026-03-12 17:47:46 +0530

    Add files via upload

---
+++ string_.py
@@ -0,0 +1,34 @@
+# #sliceing
+
+
+# string="computer"
+# print(string[:7])
+# print(string[2:])
+# print(string[2:6])
+
+
+
+# i="0123456789"
+# print(i[-6:-3])
+# print(i[1:10])
+# print(i[2:3])
+# print(i[-8:-1])
+
+
+
+
+
+##start ,end,difference
+i="0123456789"
+# print(i [: :2])
+# print(i [: :-1])
+# print(i [: :-2])
+# print(i [2: 7:2])
+print(i [-9: -2:2])
+
+
+
+
+
+
+
